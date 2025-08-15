99c169c91422f558e5f73b79618bcff0d57bcc36
94mu <zhawenbin516@gmail.com>
2025-08-15 08:30:04 +0800

    0815

--- LeetCode/0004.py
+++ LeetCode/0004.py
@@ -3,6 +3,7 @@
 https://leetcode.cn/problems/median-of-two-sorted-arrays/description/
 
 '''
+'''解法一
 def findMedianSortedArrays(nums1, nums2) -> float:
     # 计算两个列表的长度
     m = len(nums1)
@@ -34,4 +35,13 @@ def findMedianSortedArrays(nums1, nums2) -> float:
         midNum = nums1[(lenN1N2+1)//2 - 1]
     
     return midNum
+'''
 
+def findMedianSortedArrays(nums1, nums2) -> float:
+    # 计算两个数组的长度m,n
+
+    # 计算中位数的位置locMid
+
+
+    # 将两个指针pm和pn分别指向nums1[0]和nums2[0]
+    # 比较两个指针所指向的元素的大小，较小元素对应的指针+1，再比较，直至pm+pn==locMid
